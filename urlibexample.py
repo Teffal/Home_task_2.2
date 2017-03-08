1874c6a787b94d3ad2ae8bbdc52b88c7d05a25df
Konstantin <teffal@mail.ru>
2017-03-08 16:19:42 +0300

    1. Список файлов для json, написал вручную. все работает корректно. 2. Не работает предача имени файла xml, как элемента списка. Объясните почему, пожалуйста.

--- urlibexample.py
+++ urlibexample.py
@@ -1,5 +1,11 @@
-import urllib.parse
+# import urllib.parse
+#
+# url = "https://ru.wikipedia.org/wiki/%D0%9C%D0%B5%D1%82%D0%BE%D0%B4_%D0%BC%D0%BD%D0%BE%D0%B6%D0%B8%D1%82%D0%B5%D0%BB%D0%B5%D0%B9_%D0%9B%D0%B0%D0%B3%D1%80%D0%B0%D0%BD%D0%B6%D0%B0"
+# print(urllib.parse.unquote(url))
 
-url = "https://ru.wikipedia.org/wiki/%D0%9C%D0%B5%D1%82%D0%BE%D0%B4_%D0%BC%D0%BD%D0%BE%D0%B6%D0%B8%D1%82%D0%B5%D0%BB%D0%B5%D0%B9_%D0%9B%D0%B0%D0%B3%D1%80%D0%B0%D0%BD%D0%B6%D0%B0"
-print(urllib.parse.unquote(url))
+import xml.etree.ElementTree as ET
 
+tree = ET.parse('newsfr.xml')
+print(tree)
+for i in tree.iter():
+    print(i)
