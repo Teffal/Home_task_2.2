0e2743202f1ecd90f619091f13fe2dd086c2de77
Konstantin Ryzhankov <teffal@mail.ru>
2017-03-08 10:15:05 +0300

    no message

--- Home_task_2.2.py
+++ Home_task_2.2.py
@@ -1,31 +1,39 @@
 import json
-import cchardet as chardet      #cchardet=chardet, but cchardet work faste than chardet
+# import cchardet as chardet      #cchardet=chardet, but cchardet work faster than chardet
 import os
 import string
+from pprint import pprint
 
-def check_encoding(fname):
-    rawdata = open(fname, "rb").read()
-    result = chardet.detect(rawdata)['encoding']
-    return result
+list_files = [
+    ('newsafr.json', 'utf-8'),
+    ('newscy.json', 'koi8-r'),
+    ('newsfr.json', 'iso-8859-5'),
+    ('newsit.json', 'cp1251')
+]
 
-def get_file_list(our_directory, file_extension):
-    list_files = []
-    json_files = []
-    xml_files = []
-    for file in os.listdir(our_directory):
-        file_name = file #os.path.join(our_directory, file)
-        if file.endswith(file_extension):
-            list_files.append((file_name, check_encoding(file_name)))
-        # elif file.endswith(".xml"):
-        #     xml_files.append((file_name, check_encoding(file_name)))
-    return list_files
+# def check_encoding(fname):
+#     rawdata = open(fname, "rb").read()
+#     result = chardet.detect(rawdata)['encoding']
+#     return result.lower()
+#
+# def get_file_list(our_directory, file_extension):
+#     list_files = []
+#     json_files = []
+#     xml_files = []
+#     for file in os.listdir(our_directory):
+#         file_name = file #os.path.join(our_directory, file)
+#         if file.endswith(file_extension):
+#             list_files.append((file_name, check_encoding(file_name)))
+#         # elif file.endswith(".xml"):
+#         #     xml_files.append((file_name, check_encoding(file_name)))
+#     pprint(list_files)
+#     print(list_files)
+#     return list_files
 
-def get_frequency_dictionery(our_directory):
-    list_files = get_file_list(our_directory, ".json")
-    for json_file in list_files:
+def get_frequency_dictionery(list_files, our_directory):
+    for json_file, engoding_file in list_files:
         strip = string.whitespace + string.punctuation + string.digits + "\"'" + '<br>'
-        enc = json_file[1].lower().strip()
-        with open(json_file[0], 'r', encoding=enc) as f:
+        with open(json_file, 'r', encoding=engoding_file) as f:
             data = json.load(f)
             words = {}
             for cdata in data['rss']['channel']['item']:
@@ -38,7 +46,7 @@ def get_frequency_dictionery(our_directory):
                     word = word.strip(strip)
                     if len(word) > 5:
                         words[word] = words.get(word, 0) + 1
-        print_frequency_dictionery(json_file[0],words)
+        print_frequency_dictionery(json_file,words)
 
 def print_frequency_dictionery(json_file,words):
     i = 0
@@ -54,4 +62,4 @@ if __name__ == "__main__":
                           "the current working directory will be assigned):")
     if not our_directory:
         our_directory = os.getcwd()  # the current working directory: "...Home_task_2.2"
-    get_frequency_dictionery(our_directory)
+    get_frequency_dictionery(list_files, our_directory)
